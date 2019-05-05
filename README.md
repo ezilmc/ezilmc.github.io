12ee6a8e1922e1299c6c2809c6da573f696c3db7
Ram <ram.dandu728@gmail.com>
2019-05-05 23:16:04 +0530

    Reinitialised

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ezilmc.github.io" 
---
+++ precache-manifest.4d38cadba13028018aaf94d267e6e39d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b03cfe009506479987030a4a930b026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67880a78999c1e07425",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "ba5857814cef762f571c",
+    "url": "/static/css/main.764f1d7d.chunk.css"
+  },
+  {
+    "revision": "b67880a78999c1e07425",
+    "url": "/static/js/2.02fb8e4f.chunk.js"
+  },
+  {
+    "revision": "ba5857814cef762f571c",
+    "url": "/static/js/main.ad8e18fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "956758d71a2553190cc1ad3964d855b7",
+    "url": "/static/media/banner-img1.956758d7.png"
+  },
+  {
+    "revision": "956758d71a2553190cc1ad3964d855b7",
+    "url": "/static/media/banner-img2.956758d7.png"
+  },
+  {
+    "revision": "a8ec8929761cbc57843b8bf4847db1a4",
+    "url": "/static/media/bannerBg.a8ec8929.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
